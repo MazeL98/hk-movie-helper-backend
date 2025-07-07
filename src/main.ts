f20b4768e1d93a2a8481f6fd1cb869bc8ba5793d
MazeL98 <cherik0908@gmail.com>
2025-07-07 18:00:41 +0800

    fix: import koa/cors to handle cros problems

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import  Koa from 'koa';
 import  router from './router/index'
 import bodyParser from 'koa-bodyparser';
 import responseFormatter from './middlewares/responseFormatter'
+import cors from "@koa/cors";
 // 获取环境变量
 import  {ENV_CONFIG} from './config/config.default';
 
@@ -10,7 +11,11 @@ import  {ENV_CONFIG} from './config/config.default';
 const app = new Koa();
 app.use(bodyParser());
 app.use(responseFormatter);
-
+app.use(cors({
+    origin: '*',
+  allowMethods: ['GET', 'POST', 'OPTIONS'],
+  allowHeaders: ['Content-Type']
+}))
 // 注册路由
 app.use(router.routes());
 
